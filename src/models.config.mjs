b04f4bf3871f4a3d3103ac67365542bf6bec84c5
Immanence <120009219+immanencer@users.noreply.github.com>
2024-12-17 16:31:50 -0800

    Update models.config.mjs

--- src/models.config.mjs
+++ src/models.config.mjs
@@ -8,8 +8,10 @@ const models = [
   { model: "neversleep/llama-3-lumimaid-8b", rarity: "common" },
   { model: "gryphe/mythomax-l2-13b", rarity: "common" },
   { model: "google/gemini-flash-1.5-8b", rarity: "common" },
+  { model: "amazon/nova-lite-v1", rarity: "common" },
 
   // Uncommon models
+  { model: "openai/4o-mini", rarity: "uncommon" },
   { model: "anthropic/claude-3.5-haiku:beta", rarity: "uncommon" },
   { model: "meta-llama/llama-3.1-70b-instruct", rarity: "uncommon" },
   { model: "pygmalionai/mythalion-13b", rarity: "uncommon" },
@@ -18,6 +20,8 @@ const models = [
   { model: "undi95/remm-slerp-l2-13b", rarity: "uncommon" },
   { model: "meta-llama/llama-3.3-70b-instruct", rarity: "uncommon" },
   { model: "google/gemini-2.0-flash-exp:free", rarity: "uncommon" },
+  { model: "amazon/nova-micro-v1", rarity: "uncommon" },
+  { model: "thedrummer/rocinante-12b", rarity: "uncommon" },
 
   // Rare models
   { model: "eva-unit-01/eva-qwen-2.5-72b", rarity: "rare" },
@@ -25,7 +29,7 @@ const models = [
   { model: "nvidia/llama-3.1-nemotron-70b-instruct", rarity: "rare" },
   { model: "eva-unit-01/eva-llama-3.33-70b", rarity: "rare" },
   { model: "amazon/nova-pro-v1", rarity: "rare" },
-
+  { model: "inflection/inflection-3-pi", rarity: "rare" },
 
   // Legendary models
   { model: "openai/gpt-4o", rarity: "legendary" },
