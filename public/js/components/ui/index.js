6fa418ed53dcf924a244d07c78219ace4ab290b4
imannencer <34903672-imannencer@users.noreply.replit.com>
2025-01-23 04:38:37 +0000

    User checkpoint: Refactor UI components to use React and improve rendering.

--- public/js/components/ui/index.js
+++
@@ -1,9 +0,0 @@
-export { ProgressRing } from './ProgressRing';
-export { TierBadge } from './TierBadge';
-export { ActivityFeed } from './ActivityFeed';
-export { AncestryChain } from './AncestryChain';
-export { StatsDisplay } from './StatsDisplay';
-export { XAuthButton } from './XAuthButton';
-export { default as WalletButton } from './WalletButton';
-export { BurnTokenButton } from './BurnTokenButton';
-export { ViewToggle } from './ViewToggle';
