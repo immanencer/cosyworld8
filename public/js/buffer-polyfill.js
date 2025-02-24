d31efa181bf567b805b1fd03a31d239a090c2515
Immanence <120009219+immanencer@users.noreply.github.com>
2025-02-24 02:50:00 +0000

    Assistant checkpoint: Update Buffer polyfill and script imports
    Assistant generated file changes:
    - public/js/buffer-polyfill.js: Update Buffer polyfill to use browser-compatible approach
    - public/index.html: Update script imports
    
    ---
    
    User prompt:
    
    index.js:11 Uncaught ReferenceError: require is not defined
        at index.js:11:16
    (anonymous) @ index.js:11Understand this errorAI
    (index):83 Uncaught ReferenceError: buffer is not defined
        at (index):83:29
    (anonymous) @ (index):83Understand this errorAI
    buffer-polyfill.js:4 Uncaught ReferenceError: require is not defined
        at buffer-polyfill.js:4:20

--- public/js/buffer-polyfill.js
+++ public/js/buffer-polyfill.js
@@ -1,9 +1,8 @@
 
-(function() {
-  if (typeof window !== 'undefined') {
-    const buffer = require('buffer/');
-    window.Buffer = buffer.Buffer;
-    window.global = window;
-    window.process = { env: {} };
-  }
-})();
+// Import buffer from skypack (browser-compatible CDN)
+import { Buffer } from 'https://cdn.skypack.dev/buffer';
+
+// Make Buffer and other Node.js globals available
+window.Buffer = Buffer;
+window.global = window;
+window.process = { env: {} };
