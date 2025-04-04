d87fbed7a634d7b2c8689e7151b640153b9bde81
ratimics <ratichat82@gmail.com>
2025-04-03 20:29:25 -0700

    add 33 models

--- src/models.google.config.mjs
+++ src/models.google.config.mjs
@@ -1,5 +1,77 @@
 export default [
   {
+    "model": "gemini-1.0-pro-vision-latest",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-pro-vision",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-1.5-pro-latest",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-1.5-pro-001",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-1.5-pro-002",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-1.5-pro",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-1.5-flash-latest",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-001",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-1.5-flash-001-tuning",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-1.5-flash",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-002",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-8b",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-8b-001",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-8b-latest",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-8b-exp-0827",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-1.5-flash-8b-exp-0924",
+    "rarity": "common"
+  },
+  {
+    "model": "gemini-2.5-pro-exp-03-25",
+    "rarity": "legendary"
+  },
+  {
+    "model": "gemini-2.0-flash-exp",
+    "rarity": "uncommon"
+  },
+  {
     "model": "gemini-2.0-flash",
     "rarity": "uncommon"
   },
@@ -8,19 +80,55 @@ export default [
     "rarity": "uncommon"
   },
   {
-    "model": "gemini-2.0-pro",
+    "model": "gemini-2.0-flash-exp-image-generation",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-flash-lite-001",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-flash-lite",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-flash-lite-preview-02-05",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-flash-lite-preview",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-pro-exp",
     "rarity": "legendary"
   },
   {
-    "model": "gemini-2.0-pro-001",
+    "model": "gemini-2.0-pro-exp-02-05",
     "rarity": "legendary"
   },
   {
-    "model": "gemini-1.5-pro",
-    "rarity": "rare"
+    "model": "gemini-exp-1206",
+    "rarity": "uncommon"
   },
   {
-    "model": "gemini-1.5-flash",
-    "rarity": "common"
+    "model": "gemini-2.0-flash-thinking-exp-01-21",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-flash-thinking-exp",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-2.0-flash-thinking-exp-1219",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-embedding-exp-03-07",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gemini-embedding-exp",
+    "rarity": "uncommon"
   }
 ];
